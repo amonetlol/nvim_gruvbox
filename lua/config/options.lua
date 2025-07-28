35692ba03e2caec81c4dd94b0a94e08e1ac0aebd
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-07-27 21:18:34 -0300

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.number = true -- habilita numeração absoluta
-vim.opt.relativenumber = false -- desliga numeração relativa
+vim.opt.relativenumber = true -- desliga numeração relativa
